dfa5be42b8f7b63c63787088d222c657298c7a9c
Josh-shalom-06 <gjoshalom@gmail.com>
2026-07-16 11:52:30 +0530

    COntent in the files
    Change-Id: I606fde318eaae4e0dcd08c872f3d477b67cf17ef

---
+++ automation.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+
+print("=" * 50)
+print("VERSION VALIDATION REPORT")
+print("=" * 50)
+
+branch = subprocess.check_output(
+    ["git", "branch", "--show-current"]
+).decode().strip()
+
+commit = subprocess.check_output(
+    ["git", "log", "-1", "--pretty=%h - %s"]
+).decode().strip()
+
+print(f"Current Branch : {branch}")
+print(f"Latest Commit  : {commit}")
+
+print("\nValidation")
+
+files = [
+    "hello.py",
+    "version.txt"
+]
+
+for file in files:
+    if os.path.exists(file):
+        print(f"✓ {file} exists")
+    else:
+        print(f"✗ {file} missing")
+
+print("\nOverall Result : PASS")
